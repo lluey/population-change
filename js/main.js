56b161147db46b0ebd576c74ee4de02b202386b4
lluey <lluey@students.ubc.ca>
2023-03-22 17:07:19 -0700

    added relative change on click and color legend

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 d3.json("data/dataset_usa_geo_pop_list.json").then(data => {
-    console.log(data)
-    console.log("hello")
-    const choroplethMap = new ChoroplethMap({ 
+    const choroplethMap = new ChoroplethMap({
         parentElement: '#map'
       }, data);
   })
